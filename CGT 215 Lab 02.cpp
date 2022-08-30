fdc7a8ac6be64bc0ccf4f336028b8aa9caed1b10
Austin Kizer <amkizer@purdue.edu>
2022-08-30 18:30:17 -0400

    Lab 02

--- CGT 215 Lab 02.cpp
+++ CGT 215 Lab 02.cpp
@@ -1,20 +1,25 @@
-// CGT 215 Lab 02.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float A;
+	float B;
+	float X;
+
+	cout << "Greetings, I am Austin and I will solve the equation: " << endl;
+	cout << "Ax + B = 0" << endl;
+	cout << "For x" << endl << endl;
+	cout << "Please enter a value for A: ";
+	cin >> A;
+	cout << "Please enter a value for B: ";
+	cin >> B;
+	cout << endl;
+	
+	X = (-B / A);
+
+	cout << "Solving " << A << "x+" << B << "=0 for x..." << endl << endl;
+	cout << "The answer is:" << endl;
+	cout << "x=" << X;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
